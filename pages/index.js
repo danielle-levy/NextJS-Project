a9873ed12232b31f288bf60ef4c58f91bd477ce0
Danielle Levy <danielle.levy98@gmail.com>
2022-12-26 13:46:01 +0200

    init bigger project

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-// our-domain.com/
-
-function HomePage() {
-    return <h1>Home Page!</h1>
-}
-
-export default HomePage;
--- pages/news.js
+++
@@ -1,7 +0,0 @@
-// our-domain.com/news
-
-function NewsPage() {
-    return <h1>News Page!</h1>
-}
-
-export default NewsPage;
