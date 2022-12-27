834ff1c40e07f4bf68a14efc7a9d7bd86ac38046
Danielle Levy <danielle.levy98@gmail.com>
2022-12-27 18:43:47 +0200

    NewMeetupForm added

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,12 @@
+// our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
-    return <div />;
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    };
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
 export default NewMeetupPage;
