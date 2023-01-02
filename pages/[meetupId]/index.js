628c0b6f3f8587bb32d9c09d597cd15e7b7f8804
Danielle Levy <danielle.levy98@gmail.com>
2023-01-02 13:48:22 +0200

    added meetupDetail component and css module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image='https://media.licdn.com/dms/image/D4D16AQHJpqThgdDs3Q/profile-displaybackgroundimage-shrink_350_1400/0/1671816287583?e=1677715200&v=beta&t=Dt0JA8EQUeRtpduCo-6B8Gm3j7LRcypMW28vYvNtC1g'
+            title='First Meetup'
+            address='Some Street 5, Some City'
+            description='The first meetup description'
+        />
+    );
+}
+
+export default MeetupDetails;
