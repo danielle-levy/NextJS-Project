a62707aa12f8543e0f5c5eac3863f7d134289bbc
Danielle Levy <danielle.levy98@gmail.com>
2023-02-06 12:49:22 +0200

    getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,44 @@ function MeetupDetails() {
     );
 }
 
+// SSG: Static Site Generation
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId)
+
+    return {
+        props: {
+            meetupData: {
+                meetupId: meetupId,
+                image: 'https://media.licdn.com/dms/image/D4D16AQHJpqThgdDs3Q/profile-displaybackgroundimage-shrink_350_1400/0/1671816287583?e=1677715200&v=beta&t=Dt0JA8EQUeRtpduCo-6B8Gm3j7LRcypMW28vYvNtC1g',
+                title: 'First Meetup',
+                address: 'Some Street 5, Some City',
+                description: 'The first meetup description',
+            },
+        },
+    };
+}
+
 export default MeetupDetails;
