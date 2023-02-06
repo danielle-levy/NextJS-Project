b9958481e8e8e777abb8056f2a9f8509a5773d94
Danielle Levy <danielle.levy98@gmail.com>
2023-02-06 12:57:25 +0200

    api routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -31,17 +31,18 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps(context) {    
     // fetch data for a single meetup
 
     const meetupId = context.params.meetupId;
 
+    // runs during built time so that console log will show in terminal - not in browser
     console.log(meetupId)
 
     return {
         props: {
             meetupData: {
-                meetupId: meetupId,
+                id: meetupId,
                 image: 'https://media.licdn.com/dms/image/D4D16AQHJpqThgdDs3Q/profile-displaybackgroundimage-shrink_350_1400/0/1671816287583?e=1677715200&v=beta&t=Dt0JA8EQUeRtpduCo-6B8Gm3j7LRcypMW28vYvNtC1g',
                 title: 'First Meetup',
                 address: 'Some Street 5, Some City',
